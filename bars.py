a5185a3a6064b45cdce6d4a43563ec4adbcdbb5f
4pydev <4pydev@gmail.com>
2017-11-16 19:07:59 +0300

    Fixed violations of pep8

--- bars.py
+++ bars.py
@@ -10,18 +10,18 @@ def load_data(filepath):
 
 def get_biggest_bar(bars_list):
     return max(bars_list, key=lambda bar:
-        bar['properties']['Attributes']['SeatsCount'])
+               bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(bars_list):
     return min(bars_list, key=lambda bar:
-        bar['properties']['Attributes']['SeatsCount'])
+               bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(bars_list, usr_lat, usr_lon):
     return min(bars_list, key=lambda bar:
-                sqrt((usr_lat-bar['geometry']['coordinates'][0])**2) +
-                     (usr_lon-bar['geometry']['coordinates'][1])**2)
+               sqrt((usr_lat-bar['geometry']['coordinates'][0])**2) +
+               (usr_lon-bar['geometry']['coordinates'][1])**2)
 
 
 if __name__ == '__main__':
@@ -32,26 +32,29 @@ if __name__ == '__main__':
         print("The biggest bar is \n\tName: {name} \n\tAddress: {address}"
               .format(
                 name=get_biggest_bar(bars_list)['properties']
-                                                ['Attributes']['Name'],
+                                               ['Attributes']['Name'],
                 address=get_biggest_bar(bars_list)['properties']
-                                                ['Attributes']['Address']))
+                                                  ['Attributes']['Address']))
 
         print("The smallest bar is \n\tName: {name} \n\tAddress: {address}"
-            .format(
+              .format(
                 name=get_smallest_bar(bars_list)['properties']
                                                 ['Attributes']['Name'],
                 address=get_smallest_bar(bars_list)['properties']
-                                                ['Attributes']['Address']))
+                                                   ['Attributes']['Address']))
         print("------------------------------------------------")
 
         usr_lat = float(input("Enter your latitude: "))
         usr_lon = float(input("Enter your longitude: "))
         print("Closest bar to you is \n\tName: {name} \n\tAddress: {address}"
               .format(
-                name=get_closest_bar(bars_list, usr_lat, usr_lon)
-                                    ['properties']['Attributes']['Name'],
-                address=get_closest_bar(bars_list, usr_lat, usr_lon)
-                                        ['properties']['Attributes']['Address']))
+                name=get_closest_bar(bars_list,
+                                     usr_lat, usr_lon)['properties']
+                                                      ['Attributes']['Name'],
+                address=get_closest_bar(bars_list,
+                                        usr_lat, usr_lon)['properties']
+                                                         ['Attributes']
+                                                         ['Address']))
 
     except IndexError:
         print("You must enter a path to data file.")
