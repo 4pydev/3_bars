a661b4df48aaf8273fd6dfde70db5caedfe9d298
4pydev <4pydev@gmail.com>
2017-11-17 16:35:50 +0300

    Updated bars.py

--- bars.py
+++ bars.py
@@ -10,25 +10,25 @@ def load_data(filepath):
 
 def get_biggest_bar(bars_list):
     return max(bars_list, key=lambda bar:
-               bar['properties']['Attributes']['SeatsCount']), 'biggest'
+               bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(bars_list):
     return min(bars_list, key=lambda bar:
-               bar['properties']['Attributes']['SeatsCount']), 'smallest'
+               bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(bars_list, usr_lat, usr_lon):
     return min(bars_list, key=lambda bar: sqrt(
                (usr_lat-bar['geometry']['coordinates'][0])**2) +
-               (usr_lon-bar['geometry']['coordinates'][1])**2), 'closest'
+               (usr_lon-bar['geometry']['coordinates'][1])**2)
 
 
-def print_bar_info(bar):
+def print_bar_info(bar, status):
     print("The {status} bar is \n\tName: {name} \n\tAddress: {address}"
-          .format(name=bar[0]['properties']['Attributes']['Name'],
-                  address=bar[0]['properties']['Attributes']['Address'],
-                  status=bar[1]))
+          .format(name=bar['properties']['Attributes']['Name'],
+                  address=bar['properties']['Attributes']['Address'],
+                  status=status))
 
 
 if __name__ == '__main__':
@@ -37,10 +37,10 @@ if __name__ == '__main__':
         bars_list = load_data(path_to_file)['features']
 
         biggest_bar = get_biggest_bar(bars_list)
-        print_bar_info(biggest_bar)
+        print_bar_info(biggest_bar, 'biggest')
 
         smallest_bar = get_smallest_bar(bars_list)
-        print_bar_info(smallest_bar)
+        print_bar_info(smallest_bar, 'smallest')
 
         print("------------------------------------------------")
 
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         usr_lon = float(input("Enter your longitude: "))
 
         closest_bar = get_closest_bar(bars_list, usr_lat, usr_lon)
-        print_bar_info(closest_bar)
+        print_bar_info(closest_bar, 'closest')
 
     except IndexError:
         print("You must enter a path to data file.")
